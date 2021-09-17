92ba39c2cc578e5ef91ece32d8a2775926d4aba5
kenan10 <denis2014grom@gmail.com>
2021-09-17 20:56:07 +0300

    finish refactoring

--- detect.py
+++ detect.py
@@ -3,40 +3,56 @@ import pytesseract
 from PIL import Image
 
 class Detect(object):
-    def __init__(self, image, data):
+    def __init__(self, image):
         super(Detect, self).__init__()
 
         self.image = image
-        self.rotation = data["rotation"]
-        self.crop_coords = data["crop_points"]
         self.tesseract_config = r'--oem 3 --psm 13'
 
-    def to_text(self):
+    def to_text(self, rotate_deg, crop_coords):
+
         # Rotate and crop
-        self.image_rot = self.image.rotate(self.rotation)
-        self.image_cropped = self.image_rot.crop(self.crop_coords)
+        self.image_rot = self.image.rotate(rotate_deg)
+        self.image_cropped = self.image_rot.crop(crop_coords)
         
         # To text
         self.text = pytesseract.image_to_string(self.image_cropped, config=self.tesseract_config)
 
-        self.in_voltage = int(self.text[slice(0, 3)])
-        self.amper = int(self.text[5])
-        self.gear = int(self.text[slice(self.text.find("/") + 1, self.text.find("/") + 3)].strip(" "))
-        self.out_voltage = int(self.text[slice(len(self.text) - 6, len(self.text) - 3)])
+        try:
+            self.in_voltage = int(self.text[slice(0, 3)])
+        except ValueError:
+            self.in_voltage = False
 
+        try:
+            self.amper = int(self.text[5])
+        except ValueError:
+            self.amper = False
+
+        try:
+            self.gear = int(self.text[slice(self.text.find("/") + 1, self.text.find("/") + 3)].strip(" "))
+        except ValueError:
+            self.gear = False
+
+        try:
+            self.out_voltage = int(self.text[slice(len(self.text) - 6, len(self.text) - 3)])
+        except ValueError:
+            self.amper = False
+ 
         return self
 
 if __name__ == "__main__":
     image_path = sys.argv[1]
     print(image_path)
 
-    with Image.open(image_path) as image:
-        image = image
     data = { 
         "rotation": -89,
         "crop_points": [71, 121, 307, 153]
     }
 
-    indicators = Detect(image, data)
-    indicators.to_text()
-    print(indicators.in_voltage)
+    with Image.open(image_path) as image:
+        image = image
+
+        indicators = Detect(image, data)
+        indicators.to_text()
+        indicators.image_cropped.show()
+        print(indicators.text)
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import json
 import os
-from shlex import join
-from shutil import copyfile
-import time
 
-import pytesseract
 from PIL import Image
+from detect import Detect
 
 with open("config.json", "r") as read_file:
     data = json.load(read_file)
@@ -18,101 +15,50 @@ finnaly_folder = data["finnaly_folder"]
 
 tesseract_config = r'--oem 3 --psm 13'
 
-images_pathes = []
+def check_out_value(value, last_value):
+    if type(value) == int:
+        return value
+    else:
+        return last_value
+
 for image_name in os.listdir(image_folder):
-    images_pathes.append(image_folder + image_name)
+    if image_name.find("checked") == -1:
+        print("1")
+        image_path = image_folder + image_name
 
-with open("data/last.json", "w") as file:
-    json.dump({
-        "in_voltage": 0,
-        "amper": 0,
-        "gear": 0,
-        "out_voltage": 0
-    }, file)
+        folder_name = image_name[slice(image_name.find("volt_") + 5, image_name.find("."))]
+        os.mkdir(finnaly_folder + folder_name)
 
-def to_text(rot, crop_coords, image_path, finnaly_directory):
-    showed = False
+        with Image.open(image_path) as image:
+            image = Detect(image)
+            out_data = image.to_text(rotation, crop_points)
 
-    with Image.open(image_path) as image:
-        # Rotate and crop
-        image_rot = image.rotate(rot)
-        image_cropped = image_rot.crop(crop_coords)
-        image_cropped.save(f"data/finnaly/{finnaly_directory}/cropped.jpeg")
+            out_data.image_cropped.save(finnaly_folder + folder_name + "/cropped.jpeg")
+            out_data.image.save(finnaly_folder + folder_name + "/input.jpeg")
+            with open(finnaly_folder + folder_name + "/text.txt", "w") as file:
+                file.write(out_data.text)
 
-        if not showed:
-            # image_cropped.show()
-            time.sleep(0.5)
-            showed = True
-        
-        # To text
-        text = pytesseract.image_to_string(image_cropped, config=tesseract_config)
+            try:
+                with open("data/last.json", "r") as file:
+                    last_json = json.load(file)
 
-        with open("data/last.json", "r") as file:
-            last_json = json.load(file)
+                with open("data/last.json", "w") as file:
+                    json.dump({
+                        "in_voltage": check_out_value(out_data.in_voltage, last_json["in_voltage"]),
+                        "amper": check_out_value(out_data.amper, last_json["amper"]),
+                        "gear": check_out_value(out_data.gear, last_json["gear"]),
+                        "out_voltage": check_out_value(out_data.out_voltage, last_json["out_voltage"])
+                    }, file)
+                print("done!")
+            except BaseException:
+                with open("data/last.json", "w") as file:
+                    json.dump({
+                        "in_voltage": 0,
+                        "amper": 0,
+                        "gear": 0,
+                        "out_voltage": 0
+                    }, file)
 
-        try:
-            in_voltage = int(text[slice(0, 3)])
-
-            if in_voltage > 230 or in_voltage < 180:
-                in_voltage = last_json["in_voltage"]
-        except ValueError:
-            in_voltage = last_json["in_voltage"]
-
-        try:
-            amper = int(text[5])
-
-            if amper > 70 or amper < 1:
-                amper = last_json["amper"]
-        except ValueError:
-            amper = last_json["amper"]
-        
-        try:
-            gear = int(text[slice(text.find("/") + 1, text.find("/") + 3)].strip(" "))
-
-            if gear < 1 or gear > 16:
-                gear = last_json["gear"]
-        except ValueError:
-            gear = last_json["gear"]
-        
-        try:
-            out_voltage = int(text[slice(len(text) - 6, len(text) - 3)])
-
-            if out_voltage < 210 or out_voltage > 230:
-                out_voltage = last_json["out_voltage"]
-        except ValueError:
-            out_voltage = last_json["out_voltage"]
-
-        with open("data/last.json", "w") as file:
-            json.dump({
-                "in_voltage": in_voltage,
-                "amper": amper,
-                "gear": gear,
-                "out_voltage": out_voltage
-            }, file)
-
-        with open(f"data/finnaly/{finnaly_directory}/volt.txt", "w") as file:
-            file.write(text)
-
-def manage_files(images_pathes):
-    if images_pathes:
-        for image_path in images_pathes:
-            if image_path[image_path.find(".") - 1] != "c":
-                # Make directory
-                name_of_file_slice = slice(image_path.find("volt"), len(image_path))
-                name_of_file = image_path[name_of_file_slice]
-
-                format_slice = slice(name_of_file.find("."))
-
-                name_of_dir_slice = slice(name_of_file.find("volt")+5, len(name_of_file)-5)
-                name_of_dir = name_of_file[name_of_dir_slice]
-
-                os.mkdir(f"data/finnaly/{name_of_dir}")
-                copyfile(image_path, f"data/finnaly/{name_of_dir}/{name_of_file}")
-                #########################################################################
-
-                to_text(rotation, crop_points, image_path, name_of_dir)
-
-                # Mark checked files
-                # os.rename(image_path, image_path[format_slice]+"c"+".jpeg")
-                print(image_path[format_slice]+"c"+".jpeg")
-manage_files(images_pathes)
+        # Mark as checked
+        # os.rename(image_path, image_folder + image_name[slice(image_name.find("."))] + "_checked" + ".jpeg")
+        print(image_folder + image_name[slice(image_name.find("."))] + "_checked" + ".jpeg")
